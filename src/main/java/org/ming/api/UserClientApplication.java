f043501b22dbb594929c3fc24547bde1f8b09d0c
finch <21374618@qq.com>
2019-03-26 08:23:18 +0800

    将controller拆出 出口改成8989

--- src/main/java/org/ming/api/UserClientApplication.java
+++ src/main/java/org/ming/api/UserClientApplication.java
@@ -4,9 +4,11 @@ import org.ming.api.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,34 +20,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
+//@ComponentScan
 @EnableFeignClients
+//@EnableCircuitBreaker
 public class UserClientApplication {
-    @Autowired
-    HelloClient client;
-
-    @RequestMapping("/")
-    public String hello() {
-        return client.hello();
-    }
-
-    @GetMapping("users")
-    List<User> users() {
-        return client.users();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserClientApplication.class, args);
 
-    }
-
-    @FeignClient("UserService")
-    interface HelloClient {
-        @RequestMapping(value = "/", method = GET)
-        String hello();
-
-        @RequestMapping(value = "/users", method = GET)
-        List<User> users();
     }
 
 
---
+++ src/main/java/org/ming/api/UserController.java
@@ -0,0 +1,39 @@
+package org.ming.api;
+
+import org.ming.api.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    HelloClient client;
+
+    @RequestMapping("/")
+    public String hello() {
+        return client.hello();
+    }
+
+    @GetMapping("users")
+    List<User> users() {
+        return client.users();
+    }
+
+    @FeignClient("UserService")
+    interface HelloClient {
+        @RequestMapping(value = "/", method = GET)
+        String hello();
+
+        @RequestMapping(value = "/users", method = GET)
+        List<User> users();
+    }
+
+}
